148ac701e9595fb373c1af1985f6f20f22592788
Angel Rego <angelrego@gmail.com>
2023-04-14 15:31:26 +0200

    examples: lunar with HF after digging more how it works and troubleshooting
    Note in this version:
    - we probably can avoid vscreen dependency
    - from previous version, env has 'human' parameter in start so a OS screen is displayed (and therefore no vscreen required as well, maybe that dependency was only for the notebook
    - the HF DRL algorithm does not work with gymnasium (only with older versions of gym)
    - the ipbn source where this code is based on, does not work in google collab as weel, some changes must be made there (there uis a copy in my collab) but it is better to understand the problems only by looknig and thys py code and not all the remotep ipybn

--- lunar_example.py
+++ lunar_example.py
@@ -1,11 +1,14 @@
 '''
-#######################
+Example of LUNAR, with focus on simpler env understanding and different ways (custom) to RL train the model 
+
+@@@@@@@@@@@@@@@@@@@@@@@@
 CHANGES, READ FIRST!!!
-#######################
+@@@@@@@@@@@@@@@@@@@@@@@@
 
 - use gymnasium instead of gym
-- install virtualscreen as desktop
+- [CHECK if now we can remove all dependencies on this (pip & apt)] install virtualscreen as desktop [I think vscreen is optional because see my other bare gym exammple in the same folder]
 - gym env.step() parameters from latest version https://gymnasium.farama.org/api/env/
+- use render_mode human in the env creation to display
 
 
 DOCUMENTATION
@@ -21,12 +24,11 @@ sudo apt install swig cmake
 # wheel mandatory for box2d
 pip install wheel gymnasium[box2d] stable-baselines3[extra] box2d-py pyglet huggingface_sb3
 
-# For rendering
+# For rendering [REMINDER: vdisplay dependencies could be not needed now thanks to 'human' parameter in env creation]
  sudo apt install python-opengl ffmpeg ffmpeg
 pip install pyvirtualdisplay
 
 # If problems with virtualdisplay install as follows here https://github.com/ponty/pyvirtualdisplay/tree/3.0
-
 '''
 
 from stable_baselines3.common.env_util import make_vec_env
@@ -36,15 +38,24 @@ from stable_baselines3 import PPO
 # from huggingface_sb3 import load_from_hub, package_to_hub, push_to_hub
 import gymnasium as gym
 
-# ----------------------GUI----------------------------
-from pyvirtualdisplay import Display
-disp = Display(visible=True)
-disp.start()
-
 # ----------------------ENVIRONMENT--------------------
-env = gym.make("LunarLander-v2")
+'''Demonstration of gattin g parameters from the environment'''
+env = gym.make("LunarLander-v2",)  # headless mode
+env = gym.make("LunarLander-v2", render_mode="human")  # UI mode
 observation = env.reset()
-for _ in range(20):
+
+print("Observation Space Shape (vector size)", env.observation_space.shape)
+print("Sample observation", env.observation_space.sample())
+
+print("Action Space Shape (vector size)", env.action_space.n)
+print("Action Space Sample", env.action_space.sample())  # Take a random action
+
+# ---------------------CUSTOM TRAINING------------------
+'''
+Iterate on the environment and apply any custom or ad-hocs formulas for training (no specific algorithm is required)
+The idea is to demonstrate that we only need to create a custom 'SOAR' env and then we can apply our own strategy for rewards (or apply generic algorithms (probablilistic, etc) instead of the most used for RL,)
+'''
+for _ in range(500):
     action = env.action_space.sample()
     print("Action taken:", action)
     observation, reward, terminated, truncated, info = env.step(action)
@@ -52,3 +63,9 @@ for _ in range(20):
     if terminated or truncated:
         print("Environment is reset")
         observation = env.reset()
+
+# -------------------NEURAL MODE TRAINING----------------
+'''ERROR->SOLUTION: After googleing, the problem with stable_baseline algorithms is that do not so compatible with newer gym (or any gymnasium) version so install an older version of gym (not gymnasyum, to run this training) https://stackoverflow.com/questions/75108957/assertionerror-the-algorithm-only-supports-class-gym-spaces-box-box-as-acti'''
+env = gym.make('LunarLander-v2')
+model = PPO('MlpPolicy', env, verbose=1)
+model.learn(total_timesteps=int(2e4))
