beb3c72a2ac6a01a1a35a1bde8b18f46b1bc7125
Angel Rego <angelrego@gmail.com>
2023-04-21 17:38:43 +0200

    lunar: demonstration of adaptation without any ML/RL technique
    For some envarimnets we could create in the future, the structure of the actions/rewards could be maximized by other means different than classic or deep RL so this commit is a demonstration that simple set of rules 8in the future automatic geenrated) as combination, can also get the best rewards and therefore combination of the rules would match the best strategy without waiting to train the environment for hours (if the env is simulated and responses are fast, these kind of techniques could give flexibility (neural networks, minimax, other techniques) and good results.

---
+++ 2-basic_action_reward.py
@@ -0,0 +1,81 @@
+'''
+Basic example of built-in environment
+Check https://colab.research.google.com/github/huggingface/deep-rl-class/blob/master/notebooks/unit1/unit1.ipynb
+Check also gymnasium_documentation https://gymnasium.farama.org/content/basic_usage/
+
+Installation: pip install gymnasium[box2d]
+'''
+
+import gymnasium as gym
+import numpy as np
+from time import sleep
+
+
+def execute(observation, reward):
+    '''observation: x,y,vx,vy,angle,vangular,left_leg,right_leg
+    # action: 0:none, 1:to_left, 2: to_up, 3:to_right, 4: all_engines'''
+    print(f'observation: {observation} \nreward: {reward}')
+    x, y, vx, vy, a, va, L, R = observation
+    action = None
+    # Control X
+    twist = True if va < -0.20 or va > 0.20 else False
+
+    # if x < -0.15 and y < 0.5:
+    #     action = np.int64(2)
+    # elif x > 0.15 and y < 0.5:
+    #     action = np.int64(2)
+
+    # Control Angle
+    # if a < -0.30 or va < -0.30:
+    #     action = np.int64(1)
+    # elif a > 0.30 or va > 0.30:
+    #     action = np.int64(3)
+
+    if a < -0.20:
+        action = np.int64(1)
+    elif a > 0.20:
+        action = np.int64(3)
+
+    # Default
+    if action == None:
+        action = np.int64(0)
+
+    return action
+
+
+env = gym.make("LunarLander-v2", render_mode="human", gravity=-2.0)
+# env = gym.make("LunarLander-v2")
+env.action_space.seed()
+observation, info = env.reset()
+initial_action = env.action_space.sample()
+observation, reward, terminated, truncated, info = env.step(
+    env.action_space.sample())
+
+for _ in range(5000):
+    # sleep(0.1)
+    action = execute(observation, reward)
+    observation, reward, terminated, truncated, info = env.step(action)
+    x, y, vx, vy, a, va, L, R = observation
+    # for horizontal scrolling turn+up is better than single action
+    # if (action == 1 or action == 3) and observation[1] < 0.5:
+    if x < -0.2 and y < 0.5 and vy < 0.5:
+        if a > 0:
+            observation, reward, terminated, truncated, info = env.step(
+                np.int64(3))
+        observation, reward, terminated, truncated, info = env.step(
+            np.int64(2))
+
+    elif x > 0.2 and y < 0.5 and vy < 0.5:
+        if a < 0:
+            observation, reward, terminated, truncated, info = env.step(
+                np.int64(1))
+        observation, reward, terminated, truncated, info = env.step(
+            np.int64(2))
+
+    if terminated or truncated:
+        observation, info = env.reset()
+    else:
+        execute(observation, reward)
+
+
+env.close()
