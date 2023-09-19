61352c7cfb3d9a35395461c7b1e4f23d84048202
Angel Rego <angelrego@gmail.com>
2023-09-19 19:24:25 +0200

    IMPORTANT: SimpleEnv working version with only 100 iterations training convergence success
    This is a very important commit.
    
    This commit demonstrate the problem of using default PPO settings with step penalty. Depending on the step penalty values, the algorithm converges very easy (100 steps for training is enough for good results, while adding "-10" as time penalty requires 100_000 steps for succesful training.
    Te commit contains also the PPO example file for tests.
    
    Note that this is a very simple env, but advantages:
    - Easier to try things where convergence problems or failed training appear
    - Starting point for other custom environments
    - test of algorithms when deploying in production and other custom envs have problems

--- simple/2-using_PPO.py
+++ simple/2-using_PPO.py
@@ -2,11 +2,14 @@ from SimpleEnv import SimpleEnv
 from stable_baselines3 import PPO
 
 model = PPO("MlpPolicy", SimpleEnv(), verbose=1)
-model.learn(total_timesteps=10000, progress_bar=True)
+model.learn(total_timesteps=100, progress_bar=True)
 vec_env = model.get_env()
 observations = vec_env.reset()
 for r in range(1000):
-    actions, _states = model.predict(observations)
+    '''
+    Deterministic=true gives better results given same training steps.
+    '''
+    actions, _states = model.predict(observations, deterministic=True)
     observations, rewards, dones, info = vec_env.step(actions)
     # print(f'observation: {observation}')
     # print(f'Reward: {reward}')
--- simple/SimpleEnv.py
+++ simple/SimpleEnv.py
@@ -8,8 +8,12 @@ from gymnasium.envs.registration import register
 class SimpleEnv(gym.Env):
     """
     Simple environment for testing purposes.
-    Scenario starts from a random int number, the target is a given fixed number.
-    Actioms are do nothing increment and decrement
+    Instead of the slider, this is a directional problem.
+    Left to bound is success, right bound is fail
+    Note that penalty for step has been disabled because when using
+    PPO algorithm later, the learning steps are very high when the 
+    step penalty is -10 (even if low compared to success).
+    Tested with -1 also worked but better to disable. 
     """
     @staticmethod
     def register(id):
@@ -21,62 +25,63 @@ class SimpleEnv(gym.Env):
             print(f'''ENVIRONMENT CLASS: Check if you imported the class properly (eg: from security_environment import *).
                    The expected path for this registration is \"{entry_point}\" ''')
             traceback.print_exc()
-
-    MAX = 10
-    TARGET = 3
+    LOWER_BOUND = 0
+    UPPER_BOUND = 9
     MAX_STEPS = 30
 
     def __init__(self):
-        # max number [0,MAX]
-        # self.observation_space = spaces.Discrete(self.MAX)
+        super().__init__()
+        # max number [0,9]
         self.observation_space = spaces.Box(
-            low=0, high=self.MAX, shape=(1,), dtype=np.int32)
-        # 0=-1 1=0 2=+1
-        # LOOKOUT: SB3 learn has a bug, always [0,n] even i action is defined as start=-1
-        self.action_space = spaces.Discrete(3)
+            low=0, high=self.UPPER_BOUND, shape=(1,), dtype=np.int32)
+        # 0=-1 1=+1
+        # LOOKOUT: As defined in documentation SB3  Discrete is not Integer,
+        #  but NATURAL numbers {0,N} So start=-1 is not possible even if not runtime error.
+        self.action_space = spaces.Discrete(2)
         self._reward = 0
         self._steps = 0
-        # self._observation = np.random.randint(self.MAX)
 
     def _get_info(self, msg):
         return {'info': msg}
 
     def reset(self, seed=None, options=None):
+        super().reset(seed=seed, options=options)
         self._reward = 0
         self._steps = 0
-        # self._observation = np.array([np.random.randint(self.MAX)])
         self._observation = self.observation_space.sample()
-        info = self._get_info(f'System restarting. TARGET is {self.TARGET}')
+        info = self._get_info(f'System restarting')
         return self._observation, info
 
     def step(self, action):
+        observation = None
         terminated = False
         truncated = False
         self._steps += 1
-        val = self._observation[0]+action-1
-        self._observation[0] = val
-        obs = self.observation_space.sample()
-        self._reward += (-1)
-        c = self._observation[0]
-        t = self.TARGET
+        '''
+        BE CAREFUL with moderate step penalty values.
+        For instance -1 is fine with 1000 training steps
+        However -10 is really bad for 1000 or 10000 training steps
+        '''
+        # self._reward += (-1)
         info = self._get_info(f'Step count {self._steps}')
 
-        if self._steps > self.MAX_STEPS:
+        if self._steps >= self.MAX_STEPS:
             info = self._get_info('Truncate: Max steps reached')
             self._reward += (-100)
             truncated = True
-        elif val < 0 or val >= self.MAX:
+        elif action == 1 and self._observation[0] >= self.UPPER_BOUND:
             info = self._get_info('Truncate: Out of bounds')
             self._reward += (-100)
             truncated = True
-        elif c == t:
+        elif action == 0 and self._observation[0] <= self.LOWER_BOUND:
             info = self._get_info('Terminate: SUCCESS')
             self._reward += (+1000)
             terminated = True
-
+        self._observation[0] = self._observation[0] - \
+            1 if action == 0 else self._observation[0]+1
         return self._observation, self._reward, terminated, truncated, info
 
-    def render(self):
+    def render(self, mode):
         # return np.ones(self.observation_shape) * 1
         return None
 
