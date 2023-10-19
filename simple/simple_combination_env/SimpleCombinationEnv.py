afe74fb44c8e3fd39401314e98a50ebde10375ce
Angel Rego <angelrego@gmail.com>
2023-10-19 20:25:17 +0200

    envs: SimpeCombination update to improve reward management

--- simple/simple_combination_env/SimpleCombinationEnv.py
+++ simple/simple_combination_env/SimpleCombinationEnv.py
@@ -7,12 +7,18 @@ import traceback
 class SimpleEnv(gym.Env):
     '''
     Find a subset of switches to reach the solution.
-    Real usage can be find nodes to a path, etc
+    Real usage can be find nodes to a path, etc, get the right combination, etc.
+
     Example: -v-v-----v--  -> OK
     '''
+
     ACTIONS = 10
     SOLUTION = [3, 7, 1]
     MAX_STEPS = 100
+    REWARD_ACTION = 1
+    PENALTY_STEP = -1
+    REWARD_expected = 10
+    PENALTY_TIMEOUT = -10
 
     def __init__(self):
         super().__init__()
@@ -22,7 +28,7 @@ class SimpleEnv(gym.Env):
         self.action_space = spaces.Discrete(self.ACTIONS)
         self._reward = 0
         self._steps = 0
-        self._solution = []
+        self._expected = []
 
     def _get_info(self, msg):
         return {'info': msg}
@@ -31,10 +37,10 @@ class SimpleEnv(gym.Env):
         super().reset(seed=seed, options=options)
         self._reward = 0
         self._steps = 0
-        zero = self.observation_space.sample()
-        zero.fill(0)
-        self._observation = zero
-        self._solution = self.SOLUTION.copy()
+        self._expected = self.SOLUTION.copy()
+        initial = self.observation_space.sample()
+        initial.fill(0)
+        self._observation = initial
         info = self._get_info(f'System restarting')
         return self._observation, info
 
@@ -42,19 +48,20 @@ class SimpleEnv(gym.Env):
         terminated = False
         truncated = False
         self._steps += 1
-        self._reward += (-1)
-        if action in self._solution:
-            self._solution.remove(action)
-            self._observation[0] = self._observation[0] + 1/len(self.SOLUTION)
-            self._reward += 1
+        self._reward += self.PENALTY_STEP
         info = self._get_info(f'Step count {self._steps}')
+        if action in self._expected:
+            info = self._get_info(f'Reward: valid action found')
+            self._expected.remove(action)
+            self._observation[0] = self._observation[0] + 1/len(self.SOLUTION)
+            self._reward += self.REWARD_ACTION
         if self._steps >= self.MAX_STEPS:
             info = self._get_info('Truncate: Max steps reached')
-            self._reward += (-10)
+            self._reward += self.PENALTY_TIMEOUT
             truncated = True
-        elif len(self._solution) == 0:
+        elif len(self._expected) == 0:
             info = self._get_info('Terminate: SUCCESS')
-            self._reward += (+10)
+            self._reward += self.REWARD_expected
             terminated = True
         return self._observation, self._reward, terminated, truncated, info
 
