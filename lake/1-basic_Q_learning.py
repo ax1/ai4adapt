f044e829e1c285a2150aae354f23d02d251a5133
Angel Rego <angelrego@gmail.com>
2023-12-14 21:27:34 +0100

    examples: incomplete frozen lake example

---
+++ lake/1-basic_Q_learning.py
@@ -0,0 +1,4 @@
+'''
+see https://towardsdatascience.com/q-learning-for-beginners-2837b777741
+also try discount_factor near 1 to emulate goal based in the DRL algorithms we have tested previously
+'''
