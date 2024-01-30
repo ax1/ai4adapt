2275c1030e4345947b70cf4f1e00654d35f912c2
Angel Rego <angelrego@gmail.com>
2024-01-30 22:17:26 +0100

    security: PPO for 1 machine training

--- security/v2/3-using_PPO.py
+++ security/v2/3-using_PPO.py
@@ -1,13 +1,14 @@
-from security_environment import SecurityEnvironment
+# @@@@@@@@@@@@@ TODOO SWITCH BACK TO SEC_ENV NOT DUMMY
+from dorothy_security_environment import SecurityEnvironment
 from stable_baselines3 import PPO
 
-MAX_TRAINING_STEPS = 1_000
+MAX_TRAINING_STEPS = 1_00
 EXPERIMENT = f'PPO {MAX_TRAINING_STEPS} steps, default params, SB3'
 
 def train():
     # lookout: PPO default block steps aways forced to 2048 blocks, override with n_steps
     # model = PPO("MlpPolicy", SecurityEnvironment(), verbose=1, learning_rate=0.1, gamma=0.01)
-    model = PPO("MlpPolicy", SecurityEnvironment(EXPERIMENT), verbose=1, n_steps=256)
+    model = PPO("MlpPolicy", SecurityEnvironment(EXPERIMENT), verbose=1, n_steps=128)
     model.learn(total_timesteps=MAX_TRAINING_STEPS, progress_bar=False)
     return model
 
@@ -27,6 +28,7 @@ def test(model):
         # vec_env.reset()
 
 
-model = train()
-model.save(EXPERIMENT.replace(',', '_').replace(' ', '_'))
+# model = train()
+# model.save(EXPERIMENT.replace(',', '_').replace(' ', '_'))
+model = PPO.load('PPO_100_steps__default_params__SB3')
 test(model)
