fc497450128109bc10fbfe27905218f0db8d963f
Angel Rego <angelrego@gmail.com>
2024-02-02 14:59:27 +0100

    security: tests for using logging module instead of stdout

---
+++ security/v2/test_logging.py
@@ -0,0 +1,17 @@
+import logging
+from datetime import datetime
+import re
+
+filename = f"AI4ADAPT_{re.sub(r'(-|:)*','',str(datetime.now().isoformat(timespec='seconds')))}.log"
+FORMAT = '%(asctime)s %(clientip)2s %(user)s %(message)s'
+logging.basicConfig(filename=filename, filemode='a', format=FORMAT, datefmt='%Y-%m-%dT%H:%M:%S')
+d = {'clientip': '192.168.0.1', 'user': 'user1'}
+logger = logging.getLogger('tcpserver')
+logger.warning('Protocol problem: %s', 'connection reset', extra=d)
+
+
+# logging.basicConfig(filename='app.log', filemode='a', format='%(name)s - %(levelname)s - %(message)s')
+# logging.warning('This will get logged to a file')
+# logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+# logging.warning('Admin logged out')
+# logging.error("Exception occurred", exc_info=True)
