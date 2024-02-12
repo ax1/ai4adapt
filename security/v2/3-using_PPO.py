3154f04514ff7d83f2f34da98b1358a31f86eb5b
Angel Rego <angelrego@gmail.com>
2024-02-12 20:37:00 +0100

    security: PPO DRL tuned to 512 slots 50 policy updates (and Variable MAX_STEPS)
    Detached max_steps from training slots, to force early partial results learning. We will increase the max_steps once we see the RL agent learn from a complex long time attack scenario.
    
    Note also the "50" in the trainings per epoch variable. Time is our current proble, se we can afford spending time on testing strategies on the dataset per slot.

--- security/v2/3-using_PPO.py
+++ security/v2/3-using_PPO.py
@@ -1,16 +1,24 @@
-from dorothy_security_environment import SecurityEnvironment
+from security_environment import SecurityEnvironment
 from stable_baselines3 import PPO
 import os
 
 
-MAX_TRAINING_STEPS = 128
+MAX_TRAINING_STEPS = 512
 MODEL = f'PPO {MAX_TRAINING_STEPS} steps, default params, SB3'
 MODEL_FILE = MODEL.replace(',', '_').replace(' ', '_')
 
 def train():
     # lookout: PPO default block steps aways forced to 2048 blocks, override with n_steps
     # model = PPO("MlpPolicy", SecurityEnvironment(), verbose=1, learning_rate=0.1, gamma=0.01)
-    model = PPO("MlpPolicy", SecurityEnvironment(MODEL_FILE), verbose=1, n_steps=MAX_TRAINING_STEPS)
+    '''
+    DO NOT REMOVE n_epochs, in SB3 default is 10 but for finding partial solutions faster, 50 this is giving
+    much better results than tweaking the learning_rate. Epochs increase the number of times that
+    the policy is update during a slot of n_steps. Either increase epoch given many steps for finding the best
+    solution or use less epoch and less n_steps, to find partial solutions faster.
+    https://stable-baselines3.readthedocs.io/en/master/modules/ppo.html
+    https://spinningup.openai.com/en/latest/algorithms/ppo.html
+    '''
+    model = PPO("MlpPolicy", SecurityEnvironment(MODEL_FILE), verbose=1, n_epochs=10, n_steps=512)
     model.learn(total_timesteps=MAX_TRAINING_STEPS, progress_bar=False)
     return model
 
@@ -19,7 +27,7 @@ def test(model):
     print('\033[94m')
     vec_env = model.get_env()
     observations = vec_env.reset()
-    for r in range(200):
+    for r in range(50):
         '''
         Deterministic=true gives better results given same training steps.
         '''
