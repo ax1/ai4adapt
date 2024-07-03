f4d6d74c61b2998cc8769bd06dd0f38974a578e6
Angel Rego <angelrego@gmail.com>
2024-07-03 14:43:20 +0200

    security: inference in production is for recommendation service (not for execution)
    So some changes:
    - increase timeout steps. Another program should execute baesd on recommendation and reset if needed.
    - keep deterministic because it gives the same value always but based on the obs, that action is usually the right one

--- security/v2/4-inference_only.py
+++ security/v2/4-inference_only.py
@@ -5,8 +5,9 @@ The model name provided in the program input should have the same action_space a
 observation_space in the environment application.
 
 '''
-
 import sys
+from time import sleep
+
 if len(sys.argv) < 2:
     print("""
           Usage: python inference.py $model_name
@@ -23,15 +24,25 @@ import os
 MODEL = sys.argv[1]
 MODEL_FILE = MODEL.replace(',', '_').replace(' ', '_')
 dirname, filename = os.path.split(os.path.abspath(__file__))
-model = PPO.load(f'{dirname}/{MODEL_FILE}', SecurityEnvironment(MODEL))
+securityEnvironment = SecurityEnvironment(MODEL)
+
+# Tune max steps on recommendation mode
+securityEnvironment.MAX_STEPS = 1e6
+
+print(f'{dirname}/{MODEL_FILE}')
+model = PPO.load(f'{dirname}/{MODEL_FILE}', securityEnvironment)
+
 vec_env = model.get_env()
 observations = vec_env.reset()
 counter_episodes = 0
-while counter_episodes < 5:
+
+while True:
     '''
     Deterministic=true gives better results given same training steps.
     '''
+    sleep(10)
     actions, _states = model.predict(observations, deterministic=True)
+    print(f'AIADAPT RL agent: based on observations {observations[0]}, recommend executing action {actions[0]}')
     observations, rewards, dones, info = vec_env.step(actions)
     if dones:
         counter_episodes += 1
