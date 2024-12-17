8945d4812f8e12e113cbe6efeafd80c5e067b8c9
Angel Rego <angelrego@gmail.com>
2024-12-17 10:52:38 +0100

    examples: put lunar example in train()+test() format

--- lunar/sb3/lunar_with_PPO.py
+++ lunar/sb3/lunar_with_PPO.py
@@ -21,23 +21,28 @@ from stable_baselines3.common.env_util import make_vec_env
 
 ENV_NAME = "LunarLander-v2"
 LEARN_ITERATIONS = 2_500_000
-MODEL_NAME = ENV_NAME+'_PPO_'+str(LEARN_ITERATIONS)
+MODEL_NAME = f'{ENV_NAME}_PPO_{LEARN_ITERATIONS}'
 
 # Parallel environments
-
 vec_env = make_vec_env(ENV_NAME, n_envs=1)
-'''
-model = PPO("MlpPolicy", vec_env, verbose=1)
-model.learn(total_timesteps=LEARN_ITERATIONS)
-model.save(MODEL_NAME)
 
-del model  # remove to demonstrate saving and loading
-'''
-model = PPO.load(MODEL_NAME)
 
-obs = vec_env.reset()
-while True:
-    action, _states = model.predict(obs)
-    # ARF: with newer envs, done is replaced by terminated truncated
-    obs, rewards, dones, info = vec_env.step(action)
-    vec_env.render("human")
+def train():
+    model = PPO("MlpPolicy", vec_env, verbose=1)
+    model.learn(total_timesteps=LEARN_ITERATIONS, progress_bar=True)
+    model.save(MODEL_NAME)
+    # del model
+
+
+def test():
+    model = PPO.load(MODEL_NAME)
+    obs = vec_env.reset()
+    while True:
+        action, _states = model.predict(obs)
+        # ARF: with newer envs, done is replaced by terminated truncated
+        obs, rewards, dones, info = vec_env.step(action)
+        vec_env.render("human")
+
+
+# train()
+test()
