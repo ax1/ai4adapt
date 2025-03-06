bd29d610e881afd4053b940f4dd283ef1171d2f9
Angel Rego <angelrego@gmail.com>
2025-03-06 19:47:21 +0100

    security: DQN script in similar fashion as PPO

--- security/v5/2-using_DQN.py
+++ security/v5/2-using_DQN.py
@@ -3,57 +3,48 @@ from datetime import datetime
 from stable_baselines3 import DQN
 from stable_baselines3.common.evaluation import evaluate_policy
 from security_environment import SecurityEnvironment
+from stable_baselines3.common.callbacks import CheckpointCallback
 
 TARGET = 'PPC'  # TARGET IMPORTANT !!! (3 characters)
-SIMULATE = True
+SIMULATE = False
 
 MAX_TRAINING_STEPS = 2048
 TRAIN_SLOT = 64
 MODEL = f'{TARGET}, DQN {MAX_TRAINING_STEPS} steps, slot {TRAIN_SLOT}, SB3,{datetime.now().strftime("%Y%m%d_%H%M%S")}'
 MODEL_FILE = MODEL.replace(',', '_').replace(' ', '_')
 
-securityEnvironment = SecurityEnvironment(MODEL_FILE, simulate=SIMULATE, atomic=True)
-# model = DQN("MlpPolicy", securityEnvironment, verbose=1, learning_rate=0.001,
-#             target_update_interval=100, batch_size=TRAIN_SLOT)
-model = DQN("MlpPolicy", securityEnvironment, verbose=1, exploration_final_eps=0.5)
-model.learn(total_timesteps=MAX_TRAINING_STEPS, progress_bar=True)
-# model = DQN("MlpPolicy", custom_env, verbose=1, policy_kwargs=dict(net_arch=[16]), learning_rate=0.001, buffer_size=100, batch_size=8)
-# model = DQN("MlpPolicy", custom_env, verbose=1, learning_rate=0.001,
-#             train_freq=(5, 'episode'), policy_kwargs=dict(net_arch=[16]))
 
-# mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=2)
-# print(f"Mean reward: {mean_reward}, Std reward: {std_reward}")
-
-vec_env = model.get_env()
-obs = vec_env.reset()
-steps = 0
-while (steps < 100):
-    steps += 1
-    action, _states = model.predict(obs, deterministic=True)  # deterministic to test the best one
-    obs, rewards, dones, info = vec_env.step(action)
-    print(action, obs, rewards, dones)
+def train():
+    print('\n---------------TRAIN MODEL--------------------')
+    # Save model from time to time https://stable-baselines3.readthedocs.io/en/master/guide/callbacks.html#checkpointcallback
+    save_callback = CheckpointCallback(save_freq=TRAIN_SLOT, save_path="./temp/",
+                                       save_replay_buffer=True, save_vecnormalize=True)
+    securityEnvironment = SecurityEnvironment(MODEL_FILE, simulate=SIMULATE, atomic=True)
+    # model = DQN("MlpPolicy", securityEnvironment, verbose=1, learning_rate=0.001, target_update_interval=100, batch_size=TRAIN_SLOT)
+    model = DQN("MlpPolicy", securityEnvironment, verbose=1, exploration_final_eps=0.5,
+                batch_size=TRAIN_SLOT)  # final eps to force explore
+    print(model.__dict__)
+    model.learn(total_timesteps=MAX_TRAINING_STEPS, progress_bar=False, callback=save_callback)
+    return model
 
 
-'''
-import gymnasium as gym
-from stable_baselines3 import DQN
-from stable_baselines3.common.evaluation import evaluate_policy
-# from security_environment import SecurityEnvironment
-import SimpleCustomEnv
+def test(model):
+    # mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=2)
+    # print(f"Mean reward: {mean_reward}, Std reward: {std_reward}")
+    print('\n---------------TEST MODEL--------------------')
+    vec_env = model.get_env()
+    observations = vec_env.reset()
+    counter_episodes = 0
+    while counter_episodes < 20:
+        actions, _states = model.predict(observations, deterministic=True)
+        observations, rewards, dones, info = vec_env.step(actions)
+        if dones:
+            counter_episodes += 1
 
-# securityEnvironment = SecurityEnvironment('model_dqn', simulate=True, atomic=True)
-securityEnvironment = SimpleCustomEnv()
-model = DQN("MlpPolicy", securityEnvironment, verbose=1)
-model.learn(total_timesteps=2024, progress_bar=True)
 
-print('test')
-mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=2)
+# ---TRAIN---
+model = train()
+model.save(MODEL_FILE)
 
-# vec_env = model.get_env()
-# obs = vec_env.reset()
-# steps = 0
-# while (steps < 40):
-#     steps += 1
-#     action, _states = model.predict(obs, deterministic=True)
-#     obs, rewards, dones, info = vec_env.step(action)
-'''
+# ---TEST---
+test(model)
